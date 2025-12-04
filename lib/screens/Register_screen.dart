6d872f9c4d1441f8043d044730dfa5ab22db1338
novacanefrank <agya671@gmail.com>
2025-12-04 22:13:00 +0545

    Register validation

--- lib/screens/Register_screen.dart
+++ lib/screens/Register_screen.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+  
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,107 +26,189 @@ class RegisterScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 20),
-              Center(
-                child: Image.asset("assets/images/novacane.png", height: 100),
-              ),
-              const SizedBox(height: 25),
-              const Text("Get Started", style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 30),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Full name",
-                  filled: true,
-                  fillColor: const Color(0xffEFEDE7),
-                  suffixIcon: const Icon(Icons.person_outline),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+
+                // Logo
+                Center(
+                  child: Image.asset("assets/images/novacane.png", height: 100),
                 ),
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Valid email",
-                  filled: true,
-                  fillColor: const Color(0xffEFEDE7),
-                  suffixIcon: const Icon(Icons.email_outlined),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
+
+                const SizedBox(height: 25),
+                const Text(
+                  "Get Started",
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
                 ),
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Phone number",
-                  filled: true,
-                  fillColor: const Color(0xffEFEDE7),
-                  suffixIcon: const Icon(Icons.phone_android_outlined),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
-                ),
-              ),
-              const SizedBox(height: 15),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Strong password",
-                  filled: true,
-                  fillColor: const Color(0xffEFEDE7),
-                  suffixIcon: const Icon(Icons.visibility_off_outlined),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Checkbox(value: false, onChanged: (v) {}),
-                  const Expanded(
-                    child: Text.rich(
-                      TextSpan(
-                        text: "By checking the box you agree to our ",
-                        style: TextStyle(fontSize: 12),
-                        children: [
-                          TextSpan(
-                            text: "Terms and Conditions",
-                            style: TextStyle(color: Colors.red, fontSize: 12, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
+                const SizedBox(height: 30),
+
+     
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    hintText: "Full name",
+                    filled: true,
+                    fillColor: const Color(0xffEFEDE7),
+                    suffixIcon: const Icon(Icons.person_outline),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 160,
-                height: 48,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green.shade900,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                  ),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/DashboardScreen');
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Full name is required";
+                    }
+                    return null;
                   },
-                  child: const Text("Register", style: TextStyle(fontSize: 16)),
                 ),
-              ),
-              const SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Already a member? "),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/LoginScreen');
-                    },
-                    child: const Text("Login in", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 15),
+
+        
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: "Valid email",
+                    filled: true,
+                    fillColor: const Color(0xffEFEDE7),
+                    suffixIcon: const Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 40),
-            ],
+                  validator: (value) {
+                if (value == null || value.isEmpty) {
+                return "Email is required";
+                }
+                if (!value.contains('@')) {
+                return "Enter a valid email";
+             }
+             return null;
+         },
+
+           ),
+                const SizedBox(height: 15),
+
+              
+                TextFormField(
+                  controller: _phoneController,
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    hintText: "Phone number",
+                    filled: true,
+                    fillColor: const Color(0xffEFEDE7),
+                    suffixIcon: const Icon(Icons.phone_android_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Phone number required";
+                    }
+                    if (value.length < 10) {
+                      return "Enter a valid phone number";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 15),
+
+   
+                TextFormField(
+              controller: _passwordController,
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+              hintText: "Strong password",
+             filled: true,
+             fillColor: const Color(0xffEFEDE7),
+             suffixIcon: IconButton(
+            icon: Icon(
+             _isPasswordVisible
+            ? Icons.visibility_outlined
+            : Icons.visibility_off_outlined,
+           ),
+           onPressed: () {
+          setState(() {
+          _isPasswordVisible = !_isPasswordVisible; 
+        });
+      },
+    ),
+      border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide.none,
+     ),
+     ),
+    validator: (value) {
+    if (value == null || value.isEmpty) {
+      return "Password is required";
+    }
+    if (value.length < 6) {
+      return "Password must be at least 6 characters";
+    }
+    return null;
+    },
+    ),
+
+
+    const SizedBox(height: 25),
+
+         SizedBox(
+                  width: 160,
+                  height: 48,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green.shade900,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Registration Successful!"),
+                          ),
+                        );
+
+                        Navigator.pushReplacementNamed(
+                            context, '/DashboardScreen');
+                      }
+                    },
+                    child: const Text("Register", style: TextStyle(fontSize: 16)),
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Already a member? "),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/LoginScreen');
+                      },
+                      child: const Text(
+                        "Login in",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 40),
+              ],
+            ),
           ),
         ),
       ),
