0b151098cfc4af2f01de5a981a8381d8d45e625a
novacanefrank <agya671@gmail.com>
2026-01-02 12:54:05 +0545

    auth hive model

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,6 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+  static const String dbName = "app_database";
+  static const int authTypeId = 0;
+  static const String authTable = "auth_table";
+}
